8d67b455dd5084aa54e633cb3ebc47777273a6df
Antonio Navas <antonioluis_7_@hotmail.com>
2023-09-08 14:06:04 +0200

    "last commit"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ function NavBar() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/">Inicio</Nav.Link>
               {isUserActive === true ? (
                 <>
                   <isAdmin>
